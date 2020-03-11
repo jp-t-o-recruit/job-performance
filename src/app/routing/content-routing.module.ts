870c386ae8501e6a71339af51a340acae129f22d
jp-t-o-recruit <takahiro.oota.recruit@gmail.com>
2020-03-11 18:27:02 +0900

    ルーティング修正中

--- src/app/routing/content-routing.module.ts
+++ src/app/routing/content-routing.module.ts
@@ -4,9 +4,9 @@ import { routes as CONTENT_CHILDREN } from './content-routing';
 import { TopNavComponent } from '../content/top-nav/top-nav.component';
 
 export const routes: Routes = [
-  // { path: '', component: TopNavComponent, children: CONTENT_CHILDREN },
-  { path: '', redirectTo: '/top', pathMatch: 'full' },
-  { path: 'top', component: TopNavComponent, children: CONTENT_CHILDREN }
+  { path: '', component: TopNavComponent, children: CONTENT_CHILDREN },
+  // { path: '', redirectTo: '/top', pathMatch: 'full' },
+  // { path: 'top', component: TopNavComponent, children: CONTENT_CHILDREN }
 ];
 // export const routes: Routes = CONTENT_CHILDREN;
 
